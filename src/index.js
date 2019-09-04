6286915eb24758a206b43529a0965ae692f29aee
Garrett van Wageningen <ah3702@Halff.com>
2019-09-04 11:14:42 -0500

    end of chance commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 // https://www.esri.com/arcgis-blog/products/developers/mapping/design-custom-basemaps-with-the-new-arcgis-vector-tile-style-editor/
 // vector tile layer from json : https://developers.arcgis.com/javascript/latest/sample-code/layers-vectortilelayer-json/index.html
 
+// creating symbols https://www.esri.com/arcgis-blog/products/js-api-arcgis/mapping/take-advantage-of-the-javascript-symbol-playground/?rmedium=redirect&rsource=blogs.esri.com/esri/arcgis/2017/03/28/take-advantage-of-the-javascript-symbol-playground
+
 import { loadModules } from "esri-loader";
 import grayLayer from "../basemaps/layers/gray-base-layer";
 
@@ -11,9 +13,11 @@ grayLayer()
             "esri/Map",
             "esri/views/MapView",
             "esri/Basemap",
-            "esri/layers/VectorTileLayer"
+            "esri/layers/VectorTileLayer",
+            "esri/Graphic",
+            "esri/layers/GraphicsLayer"
         ])
-        .then(([Map, MapView, Basemap]) => {
+        .then(([EsriMap, MapView, Basemap, Graphic, GraphicsLayer]) => {
 
             const customBasemap = new Basemap({
                 baseLayers: [
@@ -21,7 +25,7 @@ grayLayer()
                 ]
             });
 
-            const map = new Map({
+            const map = new EsriMap({
                 basemap: customBasemap
             });
 
@@ -31,36 +35,42 @@ grayLayer()
                 center: [-98.53, 39.50], // center of USA
                 zoom: 4
             });
+
+            const graphicsLayer = new GraphicsLayer({
+                graphics: []
+            })
+
+            const capitals = new Map([
+                 ["Austin, Texas", { longitude: -97.7431, latitude: 30.2672 }],
+                 ["Boston, Massachusetts", { longitude: -71.0589, latitude: 42.3601}],
+                 ["Olympia, Washington", { longitude: -122.9007, latitude: 47.0379 }]
+            ]);
+
+            console.log(capitals);
+            // #00FFFF = aqua
+            for (let location of capitals.values()) {
+                console.log(location);
+                const pointGraphic = new Graphic({
+                    geometry: { type: "point", longitude: location.longitude, latitude: location.latitude },
+                    symbol: {
+                        type: "simple-marker",
+                        color: "#00FFFF",
+                        outline: {
+                            color: "#FFFFFF",
+                            width: 1
+                        }
+                    },
+                    popupTemplate: {
+                        title: "city state"
+                    }
+                });
+
+                graphicsLayer.add(pointGraphic);
+            }
+
+            map.layers.add(graphicsLayer);
+        }).then((view) => {
+            
         });
     });
-
-
-// loadModules([
-//     "esri/Map",
-//     "esri/views/MapView",
-//     "esri/Basemap",
-//     "esri/layers/VectorTileLayer"
-// ])
-// .then(([Map, MapView, Basemap]) => {
-//     grayLayer().then(it => console.log(it));
-
-//     const customBasemap = new Basemap({
-//         baseLayers: [
-//             grayLayer()
-//         ]
-//     });
-
-//     const map = new Map({
-//         basemap: customBasemap
-//     });
-
-//     const view = new MapView({
-//         container: "container",
-//         map: map,
-//         center: [-98.53, 39.50], // center of USA
-//         zoom: 5.5
-//     })
-// })
-// .catch(err => {
-//     console.log("error with maps: ", err);
-// });
+    
